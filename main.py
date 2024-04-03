517b259790aca6bf9417cd14757d8d641d9e9e25
stephenxu10 <stephenming228@gmail.com>
2024-04-03 13:51:52 -0500

    added neural network model

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch
+from torch import nn
 import plotly.express as px
 import pickle
-import os
 from sklearn.preprocessing import StandardScaler
 import warnings
 
+
+class RegressionNN(nn.Module):
+    def __init__(self):
+        super(RegressionNN, self).__init__()
+        self.fc1 = nn.Linear(9, 64)  # Input layer
+        self.fc2 = nn.Linear(64, 64)  
+        self.fc3 = nn.Linear(64, 1) 
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)  
+        return x
+    
+
 warnings.simplefilter("ignore")
 
-# Improved layout
 st.sidebar.title("DA SPNP Prediction Application")
 csv = st.sidebar.file_uploader("Upload CSV data for DA SPNP", type="csv")
 
@@ -21,20 +36,33 @@ if csv is not None:
 
     df = df.dropna()
     try:
-        random_forest_model = pickle.load(open("./Saved Models/random_forest_model.pkl", 'rb'))
-        
+        model = RegressionNN()
+        random_forest_model = pickle.load(open("./Saved Models/random_forest_model_v2.pkl", 'rb'))
+
+        model = RegressionNN()
+        model.load_state_dict(torch.load("./Saved Models/regression_nn_model_v2.pth"))
+        model.eval()
+
         df.columns = df.columns.str.strip()
         dates = pd.to_datetime(df['Date/Time'])
+        df = df.rename(columns={df.columns[-1]: "DA SPNP"})
 
-        df['NP15_LOAD'] = df['NP15_LOAD'].str.replace(',', '').astype('float64')
-        df['SP15_LOAD'] = df['SP15_LOAD'].str.replace(',', '').astype('float64')
+        try:
+            df['NP15_LOAD'] = df['NP15_LOAD'].str.replace(',', '').astype('float64')
+            df['SP15_LOAD'] = df['SP15_LOAD'].str.replace(',', '').astype('float64')
 
-        df['PGE_Malin_Delta'] = df['PG&E'] - df['Malin']
-        df['Load_Delta'] = df['NP15_LOAD'] - df['SP15_LOAD']
-        df['Load_Sum'] = df['NP15_LOAD'] + df['SP15_LOAD']
+            # Calculate deltas and sum
+            df['PGE_Malin_Delta'] = df['PG&E'] - df['Malin']
+            df['Load_Delta'] = df['NP15_LOAD'] - df['SP15_LOAD']
+            df['Load_Sum'] = df['NP15_LOAD'] + df['SP15_LOAD']
 
-        drop_columns = ['DA SPNP', 'Date/Time', "NP15_LOAD", "Malin", "PG&E", 'SP15_LOAD']
-        df = df.drop(columns=drop_columns, errors='ignore')
+            # Drop specified columns
+            drop_columns = ['DA SPNP', 'Date/Time', "NP15_LOAD", "Malin", "PG&E", 'SP15_LOAD']
+            df = df.drop(columns=drop_columns, errors='ignore')
+        except Exception as e:
+            pass
+
+        df = df.drop(columns=['SP15 (SOLAR_FORECAST Latest) - NP15 (SOLAR_FORECAST Latest) Maximum', 'Date/Time', 'DA SPNP'], errors='ignore')
 
         for col in df.columns:
             if df[col].dtype == 'object':
@@ -52,8 +80,10 @@ if csv is not None:
             st.write("Scaled Training Data:")
             st.dataframe(df_scaled)
 
-        y_pred = random_forest_model.predict(df_scaled)
-        
+        with torch.no_grad():
+            y_pred = model(torch.tensor(df_scaled.values, dtype=torch.float32))
+
+        y_pred = y_pred.flatten().numpy()
         df_pred = pd.DataFrame({'Date/Time': dates, 'DA SPNP': y_pred})
         if st.checkbox('Show predictions'):
             st.write("Predictions:")
